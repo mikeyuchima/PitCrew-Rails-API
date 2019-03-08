d8cc34442efad1f66dce39a504fd775b798df922
Michael Surya <40255424+mikeyuchima@users.noreply.github.com>
2019-03-08 15:06:02 -0500

    Update authentication_controller.rb

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,13 +1,2 @@
 class Api::V1::AuthenticationController < ApplicationController
-    def login_technician
-        tech = Technician.create(user_hash)
-        render json: tech
-    end
-    def login_dispatch
-        tech = Dispatch.create(user_hash)
-        render json: tech
-    end
-    def user_hash
-        params.permit(:username, :password)
-    end
 end
