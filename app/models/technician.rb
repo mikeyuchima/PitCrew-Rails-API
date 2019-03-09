255ccdf363b8ad1e26a41ad389cdf41e1ab7a88c
mikeyuchima <tomichaels@gmail.com>
2019-03-09 12:32:25 -0500

    added bcrypt, pg, updated migration files

--- app/models/technician.rb
+++ app/models/technician.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Technician < ApplicationRecord
+  has_secure_password
+
   belongs_to :ride
   has_many :tickets
+
+  validates_presence_of :username, :email, :password_digest
 end
